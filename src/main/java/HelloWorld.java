a48153e599cd878fd85838cae329235b7ca97c4c
Aniket Srivastava <2022wb86633@wilp.bits-pilani.ac.in>
2026-09-19 12:08:31 +0530

    Modified HelloWorld message

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World from Jenkins and Maven!");
+        System.out.println("Continuous Integration using Jenkins, Git and Maven!");
     }
 }
